5571061cbd37772a1c515cbb6139d91664f2567c
unknown <brianna.albo@gmail.com>
2023-01-19 18:24:17 -0500

    attempting first function to display all employees table in console.

--- db/index.js
+++ db/index.js
@@ -1,9 +1,12 @@
 
 const connector = require('./connection');
 
-displayAllEmp(){
+function displayAllEmp() {
     return this.connector.promise().query(
         "SELECT employee.id, employee.first_name, employee.last_name, role.Title, department.Department AS Department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
     
          );
 };
+
+module.exports = displayAllEmp;
+
--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-const inquirer = require('inquirer');
+const { prompt } = require('inquirer');
 const db = require('./db');
+require('console.table');
 
 function employeeMenu() {
-    inquirer
-    .prompt([
+    prompt([
         {
-            type: 'list',
-            name: 'choice',
+            type: "list",
+            name: "choice",
             message: "What would you like to do?",
             choices: [
                 {
-                    name: 'View All Employees'
+                    name: "View All Employees",
+                    // value: "View_Employees"
                 },
                 {
-                    name: 'View All Employees by Department',
+                    name: "View All Employees by Department",
+                    // value: "View_Employees_By-Department"
                 },
                 {
-                    name: 'Exit Menu'
-                }
+                    name: "Exit Menu",
+                    // value: "Exit"
+                },
             ]
         }
     ])
+//     .then(res => {
+//         let choice = res.choice;
+//         switch (choice) {
+//             case "View_Employees":
+//                 viewEmp();
+//                 break;
+//             case "View_Employees_By_Department":
+//                 console.log('here they are');
+//                 break;
+//             case "Exit":
+//                 console.log('Goodbye.');
+//                 break;
+//                 default:
+//                     console.log('???????')
+
+//         }
+//     }
+// )
 }
+
+
+// function viewEmp() {
+//     db.displayAllEmp()
+//     .then(([rows]) => {
+//         let employees = rows;
+//         console.table(employees);
+//     }
+// )
+//     .then(() => employeeMenu());
+// }
 
 employeeMenu();
